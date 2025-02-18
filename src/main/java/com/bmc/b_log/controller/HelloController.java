30ca33a51ec1df5705a1581e5b2de465ae33af10
bmchun00 <bmchun00@gmail.com>
2025-02-18 22:45:58 +0900

    init: Spring boot

---
+++ src/main/java/com/bmc/b_log/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bmc.b_log.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello, Spring Boot with Java 21!";
+    }
+}
