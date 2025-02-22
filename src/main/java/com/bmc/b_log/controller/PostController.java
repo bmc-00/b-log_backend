365ab0d956822143afeed8d0516ca4e336248a3c
bmchun00 <bmchun00@gmail.com>
2025-02-23 03:19:44 +0900

    feat: cors

--- src/main/java/com/bmc/b_log/controller/PostController.java
+++ src/main/java/com/bmc/b_log/controller/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
